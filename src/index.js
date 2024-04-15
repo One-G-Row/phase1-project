c0d7c9e9f65a68df8fe9bd86d63f80ebe1663aed
student <kmac.kimaru93@gmail.com>
2024-04-15 10:04:19 +0300

    adding DOMContentLoaded EventListener

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const ul = document.querySelector(".ul-list");
+//use dom content addEventListener to make sure the html file parses first
+document.addEventListener("DOMContentLoaded", function () {
+  //get the elements to hide the rest of the body content
+  const contentVisible = document.querySelector(".content-visible");
+  const start = document.querySelector(".start");
+  const contentHidden = document.querySelector(".content-hidden");
+});
