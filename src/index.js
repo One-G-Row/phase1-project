0ca8f0d90625b79e07ac277035aa5dacf6bdbb62
student <kmac.kimaru93@gmail.com>
2024-04-15 10:07:35 +0300

    show and hide section of page

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const contentVisible = document.querySelector(".content-visible");
   const start = document.querySelector(".start");
   const contentHidden = document.querySelector(".content-hidden");
+
+//hide content of the page until the get started button is clicked
+start.addEventListener("click", hide);
+function hide(event) {
+  event.preventDefault();
+  //hide the  content displayed as the initial content
+  contentVisible.style.display = "none";
+
+  //show the content that is hidden
+  contentHidden.style.display = "block";
+}
+
 });
